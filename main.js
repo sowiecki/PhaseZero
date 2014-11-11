984e5009289d3ced37e65a96c39939d3b817b00c
Nase00 <nase00@gmail.com>
2014-11-10 20:38:56 -0600

    Changed width properties again

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
+window.onload = resize
 window.onresize = resize
-	function resize()
+var r1440 = "700", r1080 = "600", r720 = "90%";
+function resize()
 	{
-
-	if (window.innerWidth <= 1280) {
-		document.getElementById("backing").style.width="80%"; }
-	else {
-		document.getElementById("backing").style.width="50%";
+	if (window.innerWidth <= 720) {
+		document.getElementById("backing").style.width = r720;
+		document.getElementById("gwrap").style.width = r720;
+		for(var i = 0, elems = document.getElementsByTagName("header"); i < elems.length; i++) { elems[i].style.width = r720; }
+		for(var i = 0, elems = document.getElementsByTagName("footer"); i < elems.length; i++) { elems[i].style.width = r720; }
 	}
-
-	if (window.innerWidth <= 1280) {
-		document.getElementById("gwrap").style.width="80%"; }
-	else {
-		document.getElementById("gwrap").style.width="50%";
+	else if (window.innerWidth <= 1080) {
+		document.getElementById("backing").style.width = r1080;
+		document.getElementById("gwrap").style.width = r1080;
+		for(var i = 0, elems = document.getElementsByTagName("header"); i < elems.length; i++) { elems[i].style.width = r1080; }
+		for(var i = 0, elems = document.getElementsByTagName("footer"); i < elems.length; i++) { elems[i].style.width = r1080; }
 	}
-
-	for(var i = 0, elems = document.getElementsByTagName("header"); i < elems.length; i++) {
-		if (window.innerWidth <= 1280) {
-			elems[i].style.width="80%"; }
-		else {
-			elems[i].style.width="50%";
-		}
-	};
-
-	for(var i = 0, elems = document.getElementsByTagName("footer"); i < elems.length; i++) {
-		if (window.innerWidth <= 1280) {
-			elems[i].style.width="80%"; }
-		else {
-			elems[i].style.width="50%";
-		}
-	};
+	else {
+		document.getElementById("backing").style.width = r1440;
+		document.getElementById("gwrap").style.width = r1440;
+		for(var i = 0, elems = document.getElementsByTagName("header"); i < elems.length; i++) { elems[i].style.width = r1440; }
+		for(var i = 0, elems = document.getElementsByTagName("footer"); i < elems.length; i++) { elems[i].style.width = r1440; }
+	}
 }

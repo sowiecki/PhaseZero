b3c844da8fa87793ada1b3ecb0aef4b9bc2fcee4
Nase00 <nase00@gmail.com>
2014-11-10 21:27:19 -0600

    fixed width script

--- main.js
+++ main.js
@@ -3,21 +3,18 @@ window.onresize = resize
 var r1440 = "700", r1080 = "600", r720 = "90%";
 function resize() {
 	if (window.innerWidth <= 720) {
-		document.getElementById("display_mode").innerHTML = "Display mode: 720p";
 		document.getElementById("backing").style.width = r720;
 		document.getElementById("gwrap").style.width = r720;
 		for(var i = 0, elems = document.getElementsByTagName("header"); i < elems.length; i++) { elems[i].style.width = r720; }
 		for(var i = 0, elems = document.getElementsByTagName("footer"); i < elems.length; i++) { elems[i].style.width = r720; }
 	}
 	else if (window.innerWidth <= 1080) {
-		document.getElementById("display_mode").innerHTML = "Display mode: 1080p";
 		document.getElementById("backing").style.width = r1080;
 		document.getElementById("gwrap").style.width = r1080;
 		for(var i = 0, elems = document.getElementsByTagName("header"); i < elems.length; i++) { elems[i].style.width = r1080; }
 		for(var i = 0, elems = document.getElementsByTagName("footer"); i < elems.length; i++) { elems[i].style.width = r1080; }
 	}
 	else {
-		document.getElementById("display_mode").innerHTML = "Display mode: 1440p";
 		document.getElementById("backing").style.width = r1440;
 		document.getElementById("gwrap").style.width = r1440;
 		for(var i = 0, elems = document.getElementsByTagName("header"); i < elems.length; i++) { elems[i].style.width = r1440; }
